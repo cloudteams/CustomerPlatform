b71e62a78d18fca90682cd62034729e264757b6e
dipapaspyros <dpap@epu.ntua.gr>
2015-10-19 15:56:37 +0300

    Profile wizard migrations

---
+++ profile_wizard/migrations/0007_auto_20151019_1457.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('profile_wizard', '0006_auto_20151019_1341'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='userbrandopinion',
+            name='brand',
+            field=models.CharField(max_length=16),
+        ),
+    ]
