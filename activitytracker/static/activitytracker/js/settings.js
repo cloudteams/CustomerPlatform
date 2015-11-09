fb785cfb5066fe4d0bb2f8e2b0257e3ee34e8baa
3nvi <agg.arvanitaks@gmail.com>
2015-11-09 18:01:46 +0200

    Implemented auto sync on connection. Fixed Disconnect

--- activitytracker/static/activitytracker/js/settings.js
+++ activitytracker/static/activitytracker/js/settings.js
@@ -19,20 +19,23 @@
 				success: function (response) {
 					Done();
 					$('#success-msg').removeClass('hidden').text(response);
+					if ($('.' + provider + ' .connected-app-button').length == 1) {
+						setTimeout(function(){ window.location.reload(); }, 3000);
+					}
 
 				}
 			});
 		}
 
 
-        function appFunctionHandler(provider) {
-			if ($('.' + provider + ' .connected-app-button').text() == "SynchronizeDisconnect") {
+        function appFunctionHandler(provider, button_clicked) {
+			if ($(button_clicked).attr('title') == "De-Authorize") {
 				LoadingWithBackdrop();
 				$.ajax({
 					type: "post",
 					data: {csrfmiddlewaretoken: getCookie('csrftoken')},
 					cache: false,
-					url: SERVER_URL + '/disconnect/' + provider,
+					url: '/disconnect/' + provider,
 					dataType: "text",
 					error: function (xhr, status, error) {
 						alert("Couldn't disconnect, try again")
@@ -42,11 +45,11 @@
 					}
 				});
 			}
-			else if ($('.' + provider + ' .connected-app-button').text() == "Try again") {
+			else if ($(button_clicked).attr('title') == "Retry") {
 				window.location.reload();
 			}
 			else {
-				var url = (SERVER_URL + "/login/" + provider);
+				var url = "/login/" + provider + '?next=' + BASE_URL + 'social_login/sync/' + provider;
 				window.open(url, 'newwindow', 'width=500, height=500')
 			}
 		}
