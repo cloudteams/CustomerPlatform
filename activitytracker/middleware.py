c1c30f8a602d8b7bc2d228b2bd2183dd96d43700
3nvi <agg.arvanitaks@gmail.com>
2016-12-19 15:02:12 +0200

    attempted to fix AlreadyAssociated account error

--- activitytracker/middleware.py
+++ activitytracker/middleware.py
@@ -1,11 +1,12 @@
 from social.apps.django_app.middleware import SocialAuthExceptionMiddleware
 from django.shortcuts import render, HttpResponseRedirect, redirect
-from social.exceptions import AuthCanceled
+from social.exceptions import AuthCanceled, AuthAlreadyAssociated
 
 
 class SocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     def process_exception(self, request, exception):
         if type(exception) == AuthCanceled:
             return redirect('social-login', action="AuthCancelled")
-        else:
+        elif type(exception) == AuthAlreadyAssociated:
+            return redirect('social-login', action="AuthAlreadyAssociated")
             pass
